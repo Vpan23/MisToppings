939432a3d13f462294d2d299488a6025d6eecf8d
Code <119617297+Vpan23@users.noreply.github.com>
2023-07-18 21:10:28 -0500

    finalizado

---
+++ app.js
@@ -0,0 +1,10 @@
+// const contenedor = document.getElementById('contenedor');
+
+const toppings = document.getElementsByClassName('topping');
+
+ 
+for (const topping of toppings) {
+      topping.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
+    });
+}
